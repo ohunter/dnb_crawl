28436e4b86cf47023c5625b20bd880dc95986c99
Ohunter <ohoffs@gmail.com>
2021-02-28 22:02:34 +0100

    updated to new configuration format

--- Readme.md
+++ Readme.md
@@ -22,11 +22,17 @@ While there are some drivers already included with the repository, they may at s
 The application uses a yaml file to determine which accounts are to be processed and which dates are needed. The format goes as follows:
 
 ```yaml
-- account: "####.##.#####"
-  duration:
-    - "01/2020"
-    - "01/2021"
-
+ssn: "###########"    # The user identification number
+extraction:           # Contains all the different accounts to be extracted
+  - from: "01/2020"   # The first month
+    to: "01/2021"     # The month after the last one needed
+    accounts:         # All the accounts which should be handled within this duration
+    - "####.##.#####"
 ```
 
-There has to be at least one block like the one above, but there can be as many as you want to extract as long as they are separated by an empty line. The months have to be zero padded on the left with 2 digits (ie. January is '01', but December is only '12'). The year also has to be in 4 digits. The `#`s have to be replaced by the actual account number for the program to work as well.
+The `ssn` field corresponds to the number you enter when you first want to log on to DNB. This line is optional, and if excempt you will have to enter it yourself when prompted to.
+In order to add process more than one account for a given period of time, simply add another line like the last one below it. The indentation is important, so make sure it is the same.
+In order to process different ranges for accounts, copy the lines from the `from` to the bottom and fill in the information as needed.
+The months have to be zero padded on the left with 2 digits (ie. January is '01', but December is only '12'). The year also has to be in 4 digits.
+The `#`s have to be replaced by the actual account number for the program to work as well.
+
--- main.py
+++ main.py
@@ -23,9 +23,9 @@ except ImportError:
 def num_months(m1: datetime, m2: datetime):
     return (m1.year - m2.year) * 12 + m1.month - m2.month
 
-def process_accounts(accounts):
-    for account in accounts:
-        account['months'] = list(range(*(num_months(datetime.now(), datetime.strptime(x, "%m/%Y")) for x in account['duration']), -1))
+def process_config(config):
+    for entry in config['extraction']:
+        entry['months'] = range(*(num_months(datetime.now(), datetime.strptime(x, "%m/%Y")) for x in (entry['from'], entry['to'])), -1)
 
 def resolve_env():
     """ Adds the web drivers necessary for Selenium to work at runtime """
@@ -66,7 +66,7 @@ def configure():
 
     return conf
 
-def login(driver):
+def login(driver, ssn: str = ""):
     """ Navigates the user to DNB and logs them in using a PIN and OTP combo and waits for the content to load """
 
     print("Logging in")
@@ -84,8 +84,10 @@ def login(driver):
     inp = form_1.find_element_by_xpath(".//input[@name='uid']")
     cnf = form_1.find_element_by_xpath(".//input[@id='loginFormSubmit'] | .//input[@name='Login']")
 
+    if not ssn:
+        ssn = input("Please enter your SSN for DNB: ")
     inp.clear()
-    inp.send_keys(input("Please enter your SSN for DNB: "))
+    inp.send_keys(ssn)
     cnf.click()
 
     # Wait for the necessary DOM elements to be loaded
@@ -133,77 +135,78 @@ def navigate(driver):
     sel = Select(driver.find_element_by_xpath("//select[@id='documentType'] | //select[@name='documentType']"))
     sel.select_by_value('kontoutskrift')
 
-def extract(driver, accounts):
+def extract(driver, config):
     """ Extract all the statements for the accounts given """
     print("Extracting")
 
     file_pattern = re.compile('(\\d{11})_-_(\\d{4}-\\d{2}).*')
+    for entry in config['extraction']:
+        for account in entry['accounts']:
+            months = list(entry['months'])
+            # Wait to ensure that the correct DOM elements are loaded
+            WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "documentType-button")))
+            WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "accountNumber")))
 
-    for account in accounts:
-        # Wait to ensure that the correct DOM elements are loaded
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "documentType-button")))
-        WebDriverWait(driver, 60).until(EC.presence_of_element_located((By.ID, "accountNumber")))
+            # Select the correct account
+            driver.execute_script('document.getElementById("accountNumber").style = "display: block;"')
+            sel = Select(driver.find_element_by_xpath("//select[@id='accountNumber'] | //select[@name='accountNumber']"))
+            sel.select_by_value(account.replace('.', ''))
 
-        # Select the correct account
-        driver.execute_script('document.getElementById("accountNumber").style = "display: block;"')
-        sel = Select(driver.find_element_by_xpath("//select[@id='accountNumber'] | //select[@name='accountNumber']"))
-        sel.select_by_value(account['account'].replace('.', ''))
-
-        # Iterate over the given months
-        # Goes until all the months have been extracted, even with timeouts
-        while account['months']:
-            for month in account['months']:
-                try:
-                    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "searchIntervalIndex")))
-                    driver.execute_script('document.getElementById("searchIntervalIndex").style = "display: block;"')
-                    sel = Select(driver.find_element_by_xpath("//select[@id='searchIntervalIndex'] | //select[@name='searchIntervalIndex']"))
-                    sel.select_by_value(f"{month}")
-
-                    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[@id='archiveSearchSubmit']")))
-                    driver.find_element_by_xpath("//input[@id='archiveSearchSubmit']").click()
-
-                    # Wait to ensure that the correct DOM elements are loaded
-                    WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//table//a[@href='ajax/attachment/0/kontoutskrift'] | //div[@id='userInformationView']")))
-                    
+            # Iterate over the given months
+            # Goes until all the months have been extracted, even with timeouts
+            while months:
+                for month in months:
                     try:
-                        # Click the file to download
-                        driver.find_element_by_xpath("//table//a[@href='ajax/attachment/0/kontoutskrift']").click()
-                    except NoSuchElementException:
-                        # Inform the user if it's not possible to download
-                        print(f"Could not find financial statement for {account['account']} in {driver.find_element_by_id('searchIntervalIndex-button').text}")
-                        account['months'].remove(month)
-                except TimeoutException:
-                    print(f"Timed out for {account['account']} on {driver.find_element_by_id('searchIntervalIndex-button').text}")
-                    pass
+                        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, "searchIntervalIndex")))
+                        driver.execute_script('document.getElementById("searchIntervalIndex").style = "display: block;"')
+                        sel = Select(driver.find_element_by_xpath("//select[@id='searchIntervalIndex'] | //select[@name='searchIntervalIndex']"))
+                        sel.select_by_value(f"{month}")
 
-            time.sleep(0.5)
+                        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//input[@id='archiveSearchSubmit']")))
+                        driver.find_element_by_xpath("//input[@id='archiveSearchSubmit']").click()
 
-            for file in pathlib.Path(os.getcwd()).glob('*.pdf'):
-                match = file_pattern.search(file.stem)
+                        # Wait to ensure that the correct DOM elements are loaded
+                        WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//table//a[@href='ajax/attachment/0/kontoutskrift'] | //div[@id='userInformationView']")))
+                        
+                        try:
+                            # Click the file to download
+                            driver.find_element_by_xpath("//table//a[@href='ajax/attachment/0/kontoutskrift']").click()
+                        except NoSuchElementException:
+                            # Inform the user if it's not possible to download
+                            print(f"Could not find financial statement for {account} in {driver.find_element_by_id('searchIntervalIndex-button').text}")
+                            months.remove(month)
+                    except TimeoutException:
+                        print(f"Timed out for {account} on {driver.find_element_by_id('searchIntervalIndex-button').text}")
+                        pass
 
-                if not match:
-                    continue
+                for file in pathlib.Path(os.getcwd()).glob('*.pdf'):
+                    match = file_pattern.search(file.stem)
 
-                # remove reference of the file if the file has been downloaded
-                if match.group(1) == account['account'].replace('.', '') and (month := num_months(datetime.now(), datetime.strptime(match.group(2), "%Y-%m"))) in account['months']:
-                    account['months'].remove(month)
+                    if not match:
+                        continue
 
-        combine(account)
+                    # remove reference of the file if the file has been downloaded
+                    if match.group(1) == account.replace('.', '') and (month := num_months(datetime.now(), datetime.strptime(match.group(2), "%Y-%m"))) in months:
+                        months.remove(month)
+
+            combine(account)
 
 def combine(account):
     """ Combines the downloaded pdfs into one and deletes the individual ones """
 
-    print(f"Combining for {account['account']}")
+    print(f"Combining for {account}")
 
+    # Retrieve all the files pertaining to the account
     dl_path = pathlib.Path(os.getcwd())
-    files = [x for x in dl_path.glob('*.pdf') if x.stem.startswith(account['account'].replace('.', ''))]
+    files = [x for x in dl_path.glob('*.pdf') if x.stem.startswith(account.replace('.', ''))]
 
     merger = PdfFileMerger()
 
     for file in sorted(files):
         merger.append(str(file))
 
-    merger.write(f"{account['account']}.pdf")
+    # Output the merged PDF
+    merger.write(f"{account}.pdf")
     merger.close()
 
 def cleanup():
@@ -220,21 +223,24 @@ def cleanup():
             file.unlink()
 
 def main(argv):
-    # TODO: Change from using argv directly to argparse
-    accounts = []
-    with open(sys.argv[1], 'r') as fi:
-        accounts.extend(yaml.load(fi.read(-1), Loader=Loader))
+    if len(argv) < 2:
+        print("You need to add the path to the configuration file.")
+        print("Usage: python main.py config.yaml")
+        return
 
-    process_accounts(accounts)
+    config = {}
+    with open(sys.argv[1], 'r') as fi:
+        config |= yaml.load(fi.read(-1), Loader=Loader)
+
+    process_config(config)
 
     resolve_env()
 
     # Instantiate the web browser and navigate to DNB
     driver = webdriver.Firefox(**configure())
-    
-    login(driver)
+    login(driver, config['ssn'])
     navigate(driver)
-    extract(driver, accounts)
+    extract(driver, config)
     cleanup()
 
     driver.quit()
