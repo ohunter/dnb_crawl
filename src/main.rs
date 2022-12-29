16a8f339abfc4dac50aa1942081a3f683cc15579
Ohunter <2663525+ohunter@users.noreply.github.com>
2022-12-29 00:48:24 +0000

    Continued implementation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 use clap::Parser;
 use log::{debug, error, trace};
 use std::process::Stdio;
-use std::sync::Arc;
 use std::{env, ffi::OsStr, iter::once};
 use thirtyfour::{common::capabilities::firefox::FirefoxPreferences, prelude::*};
 use tokio::{
     process::Command,
     signal::{self},
-    sync::{
-        broadcast::{channel, Receiver, Sender},
-        Mutex,
-    },
+    sync::broadcast::{channel, Receiver, Sender},
 };
 
 #[cfg(unix)]
@@ -48,30 +44,43 @@ async fn main() -> WebDriverResult<()> {
 
     let cli = Cli::parse();
 
-    let (sync, _) = channel::<Signal>(1);
+    // Used to distribute a signal between the
+    let (proc_sync_tx, mut proc_sync_rx) = channel::<Signal>(1);
+    let (task_sync_tx, _) = channel::<Signal>(1);
 
     add_geckodriver_to_path().unwrap();
 
     // Start Geckodriver
-    let gecko_fut = tokio::spawn(run_geckodriver(cli.port, sync.subscribe()));
+    let gecko_fut = tokio::spawn(run_geckodriver(
+        cli.port,
+        proc_sync_tx.subscribe(),
+        task_sync_tx.subscribe(),
+    ));
 
     let driver_fut = tokio::spawn(run_driver(
         cli.show_windows,
         cli.port,
-        sync.clone(),
-        sync.subscribe(),
+        proc_sync_tx.clone(),
+        proc_sync_tx.subscribe(),
+        task_sync_tx,
     ));
 
-    match signal::ctrl_c().await {
-        Ok(()) => {}
-        Err(err) => {
-            eprintln!("Unable to listen for shutdown signal: {err}");
+    let signal_fut = tokio::spawn(async move {
+        match signal::ctrl_c().await {
+            Ok(()) => {}
+            Err(err) => {
+                eprintln!("Unable to listen for shutdown signal: {err}");
+            }
         }
-    }
 
-    // Send a signal to shut down all the async procs
-    sync.send(Signal {}).unwrap();
+        // Send a signal to shut down all the async procs
+        proc_sync_tx.send(Signal {}).unwrap();
+    });
 
+    proc_sync_rx.recv().await.unwrap();
+
+    // There is no point in awaiting this task as it doesn't hold any resources
+    signal_fut.abort();
     driver_fut.await.unwrap().unwrap();
     gecko_fut.await.unwrap().unwrap();
 
@@ -89,7 +98,7 @@ fn setup_logger() -> Result<(), fern::InitError> {
                 message
             ))
         })
-        .level(log::LevelFilter::Trace)
+        .level(log::LevelFilter::Debug)
         .chain(std::io::stderr())
         .chain(fern::log_file("output.log")?)
         .apply()?;
@@ -148,7 +157,11 @@ fn add_geckodriver_to_path() -> Result<(), String> {
     Ok(())
 }
 
-async fn run_geckodriver(port: u16, mut sync: Receiver<Signal>) -> Result<(), String> {
+async fn run_geckodriver(
+    port: u16,
+    mut proc_sync_rx: Receiver<Signal>,
+    mut task_sync_rx: Receiver<Signal>,
+) -> Result<(), String> {
     debug!("Attempting to start geckodriver");
     let mut fut = Command::new(GECKODRIVER_EXEC)
         .args(["-p", &port.to_string()])
@@ -160,7 +173,12 @@ async fn run_geckodriver(port: u16, mut sync: Receiver<Signal>) -> Result<(), St
     debug!("Geckodriver started, waiting for shutdown signal");
 
     debug!("Waiting for signal to shutdown geckodriver");
-    sync.recv().await.unwrap();
+    proc_sync_rx.recv().await.unwrap();
+    debug!("Received process wide shutdown signal");
+
+    // debug!("Waiting for signal that the WebDriver has been shut down");
+    // task_sync_rx.recv().await.unwrap();
+    // debug!("Received WebDriver done signal");
 
     debug!("Shutdown signal received. Killing geckodriver");
     match fut.try_wait() {
@@ -179,8 +197,9 @@ async fn run_geckodriver(port: u16, mut sync: Receiver<Signal>) -> Result<(), St
 async fn run_driver(
     show_windows: bool,
     port: u16,
-    sync_tx: Sender<Signal>,
-    mut sync_rx: Receiver<Signal>,
+    proc_sync_tx: Sender<Signal>,
+    mut proc_sync_rx: Receiver<Signal>,
+    task_sync_tx: Sender<Signal>,
 ) -> Result<(), String> {
     let mut profile = FirefoxPreferences::default();
     profile.set("browser.download.folderList", 2).unwrap();
@@ -205,11 +224,9 @@ async fn run_driver(
         caps.set_headless().unwrap();
     }
 
-    let driver = Arc::new(Mutex::new(
-        WebDriver::new(&format!("http://localhost:{}", port), caps)
-            .await
-            .unwrap(),
-    ));
+    let driver = WebDriver::new(&format!("http://localhost:{port}"), caps)
+        .await
+        .unwrap();
 
     let task = tokio::spawn({
         let local_driver = driver.clone();
@@ -221,30 +238,84 @@ async fn run_driver(
             // Inform all the other tasks that the downloading has been finished
             #[allow(unused_must_use)]
             {
-                sync_tx.send(Signal {});
+                // If the result is an error that just means that that the channel has been closed already
+                proc_sync_tx.send(Signal {});
             }
             Ok(())
         }
     });
 
     debug!("Waiting for signal to shutdown task");
-    sync_rx.recv().await.unwrap();
+    proc_sync_rx.recv().await.unwrap();
 
     if !task.is_finished() {
         task.abort();
     }
 
-    driver.lock().await.clone().quit().await.unwrap();
+    driver.quit().await.unwrap();
+    task_sync_tx.send(Signal {}).unwrap();
     Ok(())
 }
 
-async fn login(driver: Arc<Mutex<WebDriver>>) -> WebDriverResult<()> {
-    let driver = driver.lock().await;
-
+async fn login(driver: WebDriver) -> WebDriverResult<()> {
     driver.goto("https://dnb.no").await?;
 
+    debug!("Awaiting the consent modal");
     let consent_modal = driver.query(By::Id("consent-modal")).first().await?;
     consent_modal.wait_until().displayed().await?;
+    debug!("Consent modal located");
+
+    debug!("Attempting to locate close button for modal");
+    let modal_close = consent_modal
+        .query(By::Tag("button"))
+        .with_class("consent-close")
+        .first()
+        .await?;
+    modal_close.wait_until().clickable().await?;
+    debug!("Close button for modal is now clickable");
+    modal_close.click().await?;
+
+    debug!("Attempting to trigger login modal");
+    let login_button = driver
+        .query(By::Tag("span"))
+        .with_text("Logg inn")
+        .first()
+        .await?;
+    login_button.wait_until().clickable().await?;
+    login_button.click().await?;
+
+    debug!("Waiting for login modal to appear");
+    let login_modal = driver.query(By::Id("dnb-modal-root")).first().await?;
+    login_modal.wait_until().displayed().await?;
+    debug!("Login modal is now displayed");
+
+    debug!("Attempting to fill in login form");
+    let login_form = login_modal.query(By::Tag("form")).first().await?;
+
+    debug!("Entering SSN into form");
+    let login_input = login_form
+        .query(By::Tag("input"))
+        .with_attribute("name", "uid")
+        .first()
+        .await?;
+    // TODO: Fix this
+    login_input.send_keys("###########").await?;
+
+    debug!("Submitting first stage login");
+    let login_button = login_form.query(By::Tag("button")).with_attribute("type", "submit").first().await?;
+    login_button.wait_until().clickable().await?;
+    login_button.click().await?;
+    debug!("First stage login form submitted");
+
+    debug!("Changing login method from BankID to PIN and OTP");
+    let login_type = driver.query(By::Tag("div")).with_id("r_state-2").first().await?;
+    login_type.wait_until().clickable().await?;
+    login_type.click().await?;
+
+    let pin_input = driver.query(By::Id("phoneCode")).first().await?;
+    let pin = rpassword::read_password().unwrap();
+
+    pin_input.send_keys(&pin).await?;
 
     Ok(())
 }
